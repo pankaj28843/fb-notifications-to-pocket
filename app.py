a912b4bc43bacfa1513a1430ccda5002b6451657
Pankaj Singh <psjinx@gmail.com>
2014-08-31 13:07:43 +0530

    A basic implementation of filtering rss feed
    Signed-off-by: Pankaj Singh <psjinx@gmail.com>

--- app.py
+++ app.py
@@ -1,11 +1,29 @@
 import os
-from flask import Flask
+from flask import Flask, request, Response
+
+from utils import filter_fb_rss_feeed
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello():
     return 'Hello World!'
+
+
+@app.route('/feed')
+def filter_fb_feed():
+    url = request.args.get('u', '')
+    if not url.startswith("https://"):
+        raise ValueError("Need a secure URL.")
+    if not url.startswith("https://www.facebook.com/feeds/notifications.php?id="):
+        raise ValueError("Need a valid FB notifications rss feed url.")
+
+    xml = filter_fb_rss_feeed(url)
+    return Response(response=xml,
+                    status=200,
+                    mimetype="application/rss+xml")
+
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
---
+++ utils.py
@@ -0,0 +1,36 @@
+import urlparse
+
+import feedparser
+from feedgen.feed import FeedGenerator
+from lxml import etree
+
+
+def filter_fb_rss_feeed(fb_notifications_feed_url):
+    parsed_feed = feedparser.parse(fb_notifications_feed_url)
+    filtered_entries = filter(
+        lambda x: ' shared a link: "' in x.title, parsed_feed.entries)
+
+    fg = FeedGenerator()
+    fg.id('https://fb-notifications-to-pocket.herokuapp.com/')
+    fg.title('Facebook Notifications to Pocket')
+    fg.author({'name': 'Pankaj Singh', 'email': 'psjinx@gmail.com'})
+    fg.description(
+        '''Filter FB notifications which contain a link and generate a new rss feed which will be used by IFTTT''')
+    fg.link(href='https://fb-notifications-to-pocket.herokuapp.com/')
+
+    for entry in filtered_entries:
+        root = etree.HTML(entry.summary_detail.value)
+        title = entry.title.split(" shared a link: ")[1].strip()[1:-2]
+        author_name = entry.title.split(" shared a link: ")[0].strip()
+        url = urlparse.parse_qs(
+            urlparse.urlparse(root.findall(".//a")[-1].attrib["href"]).query)["u"][0]
+
+        fe = fg.add_entry()
+        fe.id(entry.id)
+        fe.link(href=url)
+        fe.published(entry.published)
+        fe.author({'name': author_name})
+        fe.contributor([{'name': author_name}])
+        fe.title(title)
+
+    return fg.rss_str(pretty=True)
