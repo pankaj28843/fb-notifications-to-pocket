5253ec8d782bf5b7e1a4669a258854174d76e9ab
Pankaj Singh <psjinx@gmail.com>
2014-09-01 00:40:53 +0530

    Added instructions on home page
    Signed-off-by: Pankaj Singh <psjinx@gmail.com>

--- app.py
+++ app.py
@@ -1,19 +1,32 @@
 import os
-from flask import Flask, request, Response
+import urllib
 
+from flask import Flask, request, Response, render_template
 from utils import filter_fb_rss_feeed
+
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    return 'Hello World!'
+def home():
+    url = request.args.get('u', '').strip()
+    encoded_url = urllib.urlencode({"u": url})
+
+    if url:
+        if not url.startswith("https://"):
+            raise ValueError("Need a secure URL.")
+        if not url.startswith("https://www.facebook.com/feeds/notifications.php?id="):
+            raise ValueError("Need a valid FB notifications rss feed url.")
+
+    return render_template('home.html', url=url, encoded_url=encoded_url)
 
 
 @app.route('/feed')
 def filter_fb_feed():
     url = request.args.get('u', '')
+    if not url:
+        raise ValueError("URL can't be empty")
     if not url.startswith("https://"):
         raise ValueError("Need a secure URL.")
     if not url.startswith("https://www.facebook.com/feeds/notifications.php?id="):
@@ -28,4 +41,4 @@ def filter_fb_feed():
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=True)
