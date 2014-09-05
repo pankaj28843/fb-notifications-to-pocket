cbc552af1bbc214cdc0ce4737609cba7feaf5c5f
Pankaj Singh <psjinx@gmail.com>
2014-09-05 17:36:01 +0530

    Fixed memcached connection settings
    Signed-off-by: Pankaj Singh <psjinx@gmail.com>

--- cache.py
+++ cache.py
@@ -2,13 +2,14 @@ import os
 
 from pylibmc import Client
 
-os.environ['MEMCACHE_SERVERS'] = os.environ.get(
-    'MEMCACHIER_SERVERS', '').replace(',', ';')
-os.environ['MEMCACHE_USERNAME'] = os.environ.get('MEMCACHIER_USERNAME', '')
-os.environ['MEMCACHE_PASSWORD'] = os.environ.get('MEMCACHIER_PASSWORD', '')
+servers = os.environ.get('MEMCACHIER_SERVERS', '').split(',')
+servers = map(lambda s: s[0], map(lambda x: x.split(':'), servers))
+username = os.environ.get('MEMCACHIER_USERNAME', '')
+password = os.environ.get('MEMCACHIER_PASSWORD', '')
 
 pylibmc_client = Client(
-    os.environ['MEMCACHE_SERVERS'],
-    username=os.environ['MEMCACHE_USERNAME'],
-    password=os.environ['MEMCACHE_PASSWORD']
+    servers,
+    binary=True,
+    username=username,
+    password=password,
 )
