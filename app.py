94667b0d62b50820a9ddabb383565c37cf8b933c
Pankaj Singh <psjinx@gmail.com>
2014-11-10 16:01:37 +0530

    Disable DEBUG on production
    Signed-off-by: Pankaj Singh <psjinx@gmail.com>

--- app.py
+++ app.py
@@ -57,4 +57,4 @@ def get_links_from_twitter_feed():
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=False)
