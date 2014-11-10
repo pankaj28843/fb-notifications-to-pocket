3d898af22403d9f28724236ff1993608a66e083d
Pankaj Singh <psjinx@gmail.com>
2014-11-10 15:51:46 +0530

    Changed cache settings
    Signed-off-by: Pankaj Singh <psjinx@gmail.com>

--- cache.py
+++ cache.py
@@ -4,12 +4,8 @@ from pylibmc import Client
 
 servers = os.environ.get('MEMCACHIER_SERVERS', '').split(',')
 servers = map(lambda s: s[0], map(lambda x: x.split(':'), servers))
-username = os.environ.get('MEMCACHIER_USERNAME', '')
-password = os.environ.get('MEMCACHIER_PASSWORD', '')
 
 pylibmc_client = Client(
     servers,
     binary=True,
-    username=username,
-    password=password,
 )
